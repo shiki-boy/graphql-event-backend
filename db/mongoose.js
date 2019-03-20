a243243f5e88585d750c564eecc6e0622af3e1e6
Raghav Kaushik <raghav.kaushik611@gmail.com>
2019-03-20 20:51:21 +0530

    connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,23 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-// var graphqlDBConn = mongoose.createConnection('mongodb://localhost:27017/graphQLDBevents', {
-// useCreateIndex: true,
-// useNewUrlParser: true
-// })
-let localDB;
-try {
-  var graphqlDBConn = mongoose.createConnection(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-itf8s.mongodb.net/graphQLDBevents?retryWrites=true/`, {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  })
-} catch (error) {
-  var graphqlDBConn = mongoose.createConnection('mongodb://localhost:27017/graphQLDBevents', {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  })
-
-}
+var graphqlDBConn = mongoose.createConnection(process.env.MONGO_URI || "mongodb://localhost:27017/graphQLDBevents", {
+  useCreateIndex: true,
+  useNewUrlParser: true
+})
 
 module.exports = {
   graphqlDBConn
